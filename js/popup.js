9dc8fbbb42e6ace20d1b8a1e09b33608797f3df3
jc3213 <jc3213@users.noreply.github.com>
2020-08-29 22:52:28 +0800

    pre-implement for task option
    https://github.com/jc3213/download_with_aria2-chromium/commit/08d144750884d068c9a0ccb6b82b214914eea45f

--- js/popup.js
+++ js/popup.js
@@ -97,7 +97,6 @@ function printMainFrame() {
         if (result.bittorrent) {
             if (result.bittorrent.info) {
                 var taskName = result.bittorrent.info.name;
-                var showButton = ' <span id="show_btn" class="button">👁️</span>';
             }
             var numSeeders = ' (' + result.numSeeders + ' ' + window['task_bit_seeders'] + ')';
             var uploadSpeed = ', ⇧: ' + bytesToFileSize(result.uploadSpeed) + '/s';
@@ -109,10 +108,9 @@ function printMainFrame() {
         taskName = taskName || result.files[0].path.split('/').pop() || taskUrl;
         numSeeders = numSeeders || '';
         uploadSpeed = uploadSpeed || '';
-        showButton = showButton || '';
         copyButton = copyButton || '';
         return '<div class="taskInfo" gid="' + result.gid + '" status="' + result.status + '" name="' + taskName + '">'
-        +          '<div><span class="title">' + taskName + '</span>' + copyButton + showButton + ' <span id="remove_btn" class="button">❌</span></div>'
+        +          '<div><span class="title">' + taskName + '</span>' + copyButton + ' <span id="option_btn" class="button">⚙️</span> <span id="remove_btn" class="button">❌</span></div>'
         +          '<div>' + window['task_download_size'] + ': ' + completedLength + '/' + totalLength + ', ' + window['task_estimated_time'] + ': ' + estimatedTime + '</div>'
         +          '<div class="' + result.status + '_info">' + window['task_connections'] + ': ' + result.connections + numSeeders + ', ⇩: ' + downloadSpeed + '/s' + uploadSpeed + '</div>'
         +          '<div id="progress_bar" class="progress ' + result.status + '_bar"><span id="progress_bar" class="' + result.status + '" style="width: ' + completeRatio + '">' + completeRatio + '</span></div>'
--- js/taskmgr.js
+++ js/taskmgr.js
@@ -1,9 +1,8 @@
 $('div.taskQueue').on('click', (event) => {
     var taskInfo = $('div.taskInfo').has($(event.target));
     var status = taskInfo.attr('status'), gid = taskInfo.attr('gid'), name = taskInfo.attr('name');
-    if (event.target.id === 'show_btn') {
-        clearInterval(taskManager);
-        $('#showTaskFiles').show();
+    if (event.target.id === 'option_btn') {
+        $('#taskDetails').show();
         printTaskFiles(gid);
         taskManager = setInterval(() => {
             printTaskFiles(gid);
@@ -25,7 +24,7 @@ $('div.taskQueue').on('click', (event) => {
         showNotification(window['warn_url_copied'], url);
     }
 
-    function removeTask(status, gid, name) {
+    function removeTask(status, gid) {
         if (['active', 'waiting', 'paused'].includes(status)) {
             var method = 'aria2.forceRemove';
         }
@@ -38,7 +37,7 @@ $('div.taskQueue').on('click', (event) => {
         jsonRPCRequest({'method': method, 'gid': gid});
     }
 
-    function toggleTask(status, gid, name) {
+    function toggleTask(status, gid) {
         if (['active', 'waiting'].includes(status)) {
             var method = 'aria2.pause';
         }
@@ -58,26 +57,45 @@ $('div.taskQueue').on('click', (event) => {
         jsonRPCRequest(
             {'method': 'aria2.tellStatus', 'gid': gid},
             (result) => {
-                var taskName = result.bittorrent.info.name;
-                var taskFiles = result.files.map((item, index) => item = '<tr><td>'
-                +   multiDecimalNumber(index + 1, 3) + '</td><td style="text-align: left;">'
-                +   item.path.split('/').pop() + '</td><td>'
-                +   bytesToFileSize(item.length) + '</td><td>'
-                +   ((item.completedLength / item.length * 10000 | 0) / 100).toString() + '%</td></tr>'
-                );
-                $('#showTaskFiles').html('<div id="showTask" class="title status button ' + result.status + '">' + taskName + '</div><hr>'
-                +   '<div id="showFiles"><table>'
-                +       '<tr><td>' + window['task_file_index'] + '</td><td>' + window['task_file_name'] + '</td><td>' + window['task_download_size'] + '</td><td>' + window['task_complete_ratio'] + '</td></tr>'
-                +       taskFiles.join('')
-                +   '</table></div>');
+                if (result.bittorrent) {
+                    if (result.bittorrent.info) {
+                        var taskName = result.bittorrent.info.name;
+                    }
+                    detailedTorrent(result);
+                }
+                else {
+                    var taskUrl = result.files[0].uris[0].uri;
+                    detailedDownload(result);
+                }
+                taskName = taskName || result.files[0].path.split('/').pop() || taskUrl;
+                $('#taskName').html('<div class="title status button ' + result.status + '">' + taskName + '</div>');
             }
         );
     }
+
+    function detailedTorrent(result) {
+        var taskFiles = result.files.map((item, index) => item = '<tr><td>'
+        +           multiDecimalNumber(index + 1, 3) + '</td><td style="text-align: left;">'
+        +           item.path.split('/').pop() + '</td><td>'
+        +           bytesToFileSize(item.length) + '</td><td>'
+        +           ((item.completedLength / item.length * 10000 | 0) / 100).toString() + '%</td></tr>'
+        );
+        $('#taskOption').html();
+        $('#taskFiles').html('<table>'
+        +           '<tr><td>' + window['task_file_index'] + '</td><td>' + window['task_file_name'] + '</td><td>' + window['task_download_size'] + '</td><td>' + window['task_complete_ratio'] + '</td></tr>'
+        +           taskFiles.join('')
+        +           '</table></div>');
+    }
+
+    function detailedDownload(result) {
+        $('#taskOption').html();
+        $('#taskFiles').empty();
+    }
 });
 
-$('#showTaskFiles').on('click', '#showTask', (event) => {
+$('#taskName').on('click', (event) => {
     clearInterval(taskManager);
-    $('#showTaskFiles').hide();
+    $('#taskDetails').hide();
 });
 
 var taskManager;
