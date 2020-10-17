2f6a34c9e36d6c119c7d27be9bf22b2ac2995a8b
jc3213 <jc3213@users.noreply.github.com>
2020-10-18 07:37:47 +0800

    some clean up

--- modules/newTask/js.js
+++ modules/newTask/js.js
@@ -1,3 +1,7 @@
+window.addEventListener('load', (event) => {
+    document.getElementById('taskProxy').value = localStorage.getItem('allproxy') || '';
+});
+
 document.getElementById('setProxy').addEventListener('click', (event) => {
     document.getElementById('taskProxy').disabled = !document.getElementById('taskProxy').disabled;
 });
@@ -7,12 +11,4 @@ document.getElementById('submit_btn').addEventListener('click', (event) => {
     var proxy = document.getElementById('setProxy').checked ? document.getElementById('taskProxy').value : '';
     var url = document.getElementById('taskBatch').value.split('\n').forEach(item => { if (item !== '') downWithAria2({'url': item, 'referer': referer, 'proxy': proxy}) });
     window.parent.window.postMessage('newTaskWindow');
-});
-
-window.addEventListener('load', (event) => {
-    document.getElementById('taskReferer').value = '';
-    document.getElementById('taskBatch').value = '';
-    document.getElementById('setProxy').checked = false;
-    document.getElementById('taskProxy').value = localStorage.getItem('allproxy') || '';
-    document.getElementById('taskProxy').disabled = true;
 });
