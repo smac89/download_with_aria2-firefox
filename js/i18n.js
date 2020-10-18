276f698f0f2e12865c4fc999dbebb3b707ff0a34
jc3213 <jc3213@users.noreply.github.com>
2020-10-18 10:22:11 +0800

    Delete i18n.js

--- js/i18n.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('[i18n]').forEach(item => item.innerHTML = browser.i18n.getMessage(item.innerHTML));
-
-document.querySelectorAll('[i18n_title]').forEach(item => item.title = browser.i18n.getMessage(item.title));
-
-[
-    'warn_aria2_version', 'warn_url_copied'
-].forEach(item => window[item] = browser.i18n.getMessage(item));
