22cb81672a90e375d7d14ab08943b40d1d749293
jc3213 <jc3213@users.noreply.github.com>
2020-10-20 18:43:05 +0800

    optimized fileSize check work around #7

--- background.js
+++ background.js
@@ -16,7 +16,7 @@ browser.downloads.onCreated.addListener((item) => {
         return;
     }
     var worker = {'url': item.url, 'saveAs': false, 'filename': item.filename.match(/[^\\]+$/i)[0]};
-    var search = requests.indexOf(item.url);
+    var search = requests.indexOf(worker.url);
     if (search !== -1) {
         requests = [...requests.slice(0, search), ...requests.slice(search + 1)];
         return;
@@ -26,61 +26,57 @@ browser.downloads.onCreated.addListener((item) => {
             var capture = (localStorage.getItem('capture') | 0);
             if (capture > 0) {
                 if (item.referrer) {
-                    initiateCapture(capture, item);
+                    captureAdd(capture, item);
                 }
                 else {
                     browser.tabs.query({'active': true, 'currentWindow': true}, (tabs) => {
                         item.referrer = tabs[0].url;
-                        initiateCapture(capture, item);
+                        captureAdd(capture, item);
                     });
                 }
             }
             else {
-                requests.push(item.url);
-                browser.downloads.download(worker);
+                resumeDownload(worker);
             }
         });
     });
 
-    function initiateCapture(capture, item) {
+    function captureAdd(capture, item) {
+        item.domain = domainFromUrl(item.referrer);
+        var ignored = localStorage.getItem('ignored');
+        if (ignored && ignored.includes(item.domain)) {
+            return resumeDownload(worker);
+        }
+        var monitored = localStorage.getItem('monitored');
+        if (monitored && monitored.includes(item.domain)) {
+            return aria2Download(item);
+        }
+        item.fileExt = item.filename.split('.').pop();
+        var fileExt = localStorage.getItem('fileExt');
+        if (fileExt && fileExt.includes(item.fileExt)) {
+            return aria2Download(item);
+        }
+        var fileSize = (localStorage.getItem('fileSize') | 0);
         var xhr = new XMLHttpRequest();
         xhr.open('HEAD', item.url, true);
         xhr.onload = (event) => {
             item.fileSize = xhr.getResponseHeader('Content-Length');
-            captureAdd(capture, item);
+            if (fileSize > 0 && item.fileSize >= fileSize) {
+                aria2Download(item);
+            }
+            else {
+                resumeDownload(worker);
+            }
         };
         xhr.send();
     }
 
-    function captureAdd(capture, item) {
-        var domain = domainFromUrl(item.referrer);
-        var check = captureCheck(domain, item.filename.split('.').pop(), item.fileSize);
-        if (capture === 2 || check) {
-            downWithAria2({'url': item.url, 'referer': item.referrer, 'domain': domain, 'filename': item.filename.match(/[^\\]+$/i)[0], 'path': item.filename.replace(/[^\\]+$/i, '')});
-        }
-        else {
-            requests.push(item.url);
-            browser.downloads.download(worker);
-        }
+    function resumeDownload(worker) {
+        requests.push(worker.url);
+        browser.downloads.download(worker);
     }
 
-    function captureCheck(domain, ext, size) {
-        var ignored = localStorage.getItem('ignored');
-        if (ignored && ignored.includes(domain)) {
-            return false;
-        }
-        var monitored = localStorage.getItem('monitored');
-        if (monitored && monitored.includes(domain)) {
-            return true;
-        }
-        var fileExt = localStorage.getItem('fileExt');
-        if (fileExt && fileExt.includes(ext)) {
-            return true;
-        }
-        var fileSize = (localStorage.getItem('fileSize') | 0);
-        if (fileSize > 0 && size >= fileSize) {
-            return true;
-        }
-        return false;
+    function aria2Download(item) {
+        downWithAria2({'url': item.url, 'referer': item.referrer, 'domain': item.domain, 'filename': item.filename.match(/[^\\]+$/i)[0], 'path': item.filename.replace(/[^\\]+$/i, '')});
     }
 });
