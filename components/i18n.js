28564081773a9628dc21b28094fcf049827a7733
jc3213 <jc3213@users.noreply.github.com>
2020-10-21 09:21:44 +0800

    update i18n logic

--- components/i18n.js
+++ components/i18n.js
@@ -1,6 +1,15 @@
-document.querySelectorAll('[i18n]').forEach(item => item.innerHTML = browser.i18n.getMessage(item.innerHTML));
+document.querySelectorAll('[i18n]').forEach(item => {
+    var message = item.getAttribute('i18n');
+    var i18n = browser.i18n.getMessage(message);
+    var textNode = document.createTextNode(i18n);
+    item.appendChild(textNode);
+});
 
-document.querySelectorAll('[i18n_title]').forEach(item => item.title = browser.i18n.getMessage(item.title));
+document.querySelectorAll('[i18n_title]').forEach(item => {
+    var message = item.getAttribute('i18n_title');
+    var i18n = browser.i18n.getMessage(message);
+    item.title = i18n;
+});
 
 [
     'warn_aria2_version', 'warn_url_copied'
