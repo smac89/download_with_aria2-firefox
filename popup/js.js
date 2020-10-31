2403048c8a0e5513da0f773fe144357f4177f9ba
jc3213 <jc3213@users.noreply.github.com>
2020-10-31 10:21:57 +0800

    reverted: split taskInfo into /modules/template

--- popup/js.js
+++ popup/js.js
@@ -1,51 +1,34 @@
 window.addEventListener('message', (event) => {
-    if (event.data.method === 'close') {
-        document.getElementById(event.data.id).remove();
-    }
-    else if (event.data.method === 'manager') {
-        var gid = event.data.id;
-        openModuleWindow({'name': 'taskMgr', 'id': 'taskMgrWindow', 'load': (event) => event.target.contentWindow.postMessage(gid)});
-    }
+    document.getElementById(event.data).remove();
+    modules.forEach(item => { if (item.win === event.data) document.getElementById(item.id).classList.remove('checked'); });
 });
 
 var modules = [
-    {'button': 'newTask_btn', 'name': 'newTask', 'id': 'newTaskWindow'}, 
-    {'button': 'options_btn', 'name': 'options', 'id': 'optionsWindow'}
+    {'id': 'newTask_btn', 'name': 'newTask', 'win': 'newTaskWindow'}, 
+    {'id': 'options_btn', 'name': 'options', 'win': 'optionsWindow'}
 ];
-modules.forEach(item => document.getElementById(item.button).addEventListener('click', (event) => initialModules(event.target, item)));
+modules.forEach(item => document.getElementById(item.id).addEventListener('click', (event) => initialModules(event.target, item)));
 
 function initialModules(element, module) {
     if (element.classList.contains('checked')) {
-        document.getElementById(module.id).remove();
+        document.getElementById(module.win).remove();
     }
     else {
-        openModuleWindow(module)
-    }
-    element.classList.toggle('checked');
-}
-
-function openModuleWindow(module) {
-    var iframe = document.createElement('iframe');
-    iframe.id = module.id;
-    iframe.src = '/modules/' + module.name + '/index.html';
-    document.body.appendChild(iframe);
-    if (module.load) {
-        iframe.addEventListener('load', module.load);
-    }
-    if (module.value) {
-        iframe.value = module.value;
-    }
-    if (module.target) {
-        document.getElementById(module.target).appendChild(iframe);
-    }
-    else {
+        var iframe = document.createElement('iframe');
+        iframe.id = module.win;
+        iframe.src = '/modules/' + module.name + '/index.html';
+        if (module.load) {
+            iframe.addEventListener('load', module.load);
+        }
         document.body.appendChild(iframe);
     }
+    element.classList.toggle('checked');
 }
 
 var taskTabs = ['active_btn', 'waiting_btn', 'stopped_btn'];
 var taskQueues = ['activeQueue', 'waitingQueue', 'stoppedQueue'];
 taskTabs.forEach((item, index) => document.getElementById(item).addEventListener('click', (event) => toggleTaskQueue(event.target, item, taskQueues[index])));
+taskQueues.forEach(item => document.getElementById(item).addEventListener('click', (event) => toggleTaskManager(event.target)));
 
 function toggleTaskQueue(element, active, activeTab) {
     if (element.classList.contains('checked')) {
@@ -59,11 +42,62 @@ function toggleTaskQueue(element, active, activeTab) {
     element.classList.toggle('checked');
 }
 
-document.getElementById('purdge_btn').addEventListener('click', (event) => {
-    jsonRPCRequest(
-        {'method': 'aria2.purgeDownloadResult'},
-        (result) => taskQueues.forEach(item => document.getElementById(item).innerHTML = '')
+function toggleTaskManager(element, task) {
+    document.querySelectorAll('div.taskInfo').forEach(item => { if (item.contains(element)) task = {'gid': item.getAttribute('gid'), 'status': item.getAttribute('status')}; })
+    if (element.id === 'remove_btn') {
+        removeTask(task.gid, task.status);
+    }
+    if (element.id === 'invest_btn') {
+        initialModules(element, {'name': 'taskMgr', 'win': 'taskMgrWindow', 'load': (event) => event.target.contentWindow.postMessage(task.gid)});
+    }
+    if (element.id === 'retry_btn') {
+        retryTask(task.gid);
+    }
+    if (element.id === 'progress_btn') {
+        toggleTask(task.gid, task.status);
+    }
+}
+
+function removeTask(gid, status) {
+    if (['active', 'waiting', 'paused'].includes(status)) {
+        var method = 'aria2.forceRemove';
+    }
+    else if (['complete', 'error', 'removed'].includes(status)) {
+        method = 'aria2.removeDownloadResult';
+    }
+    else {
+        return;
+    }
+    jsonRPCRequest({'method': method, 'gid': gid});
+}
+
+function toggleTask(gid, status) {
+    if (['active', 'waiting'].includes(status)) {
+        var method = 'aria2.pause';
+    }
+    else if (status === 'paused') {
+        method = 'aria2.unpause';
+    }
+    else {
+        return;
+    }
+    jsonRPCRequest({'method': method, 'gid': gid});
+}
+
+function retryTask(gid) {
+    jsonRPCRequest([
+            {'method': 'aria2.getFiles', 'gid': gid},
+            {'method': 'aria2.getOption', 'gid': gid},
+        ], (files, options) => {
+            jsonRPCRequest({'method': 'aria2.removeDownloadResult', 'gid': gid}, () => {
+                downWithAria2({'url': files[0].uris[0].uri, 'options': options});
+            });
+        }
     );
+}
+
+document.getElementById('purdge_btn').addEventListener('click', (event) => {
+    jsonRPCRequest({'method': 'aria2.purgeDownloadResult'});
 });
 
 function printMainFrame() {
@@ -98,30 +132,35 @@ function printMainFrame() {
             {'method': 'aria2.tellWaiting', 'index': [0, globalWaiting]},
             {'method': 'aria2.tellStopped', 'index': [0, globalStopped]},
         ], (activeQueue, waitingQueue, stoppedQueue) => {
-            activeQueue.forEach(item => printTaskInfo(item, 'activeQueue'));
-            waitingQueue.forEach(item => printTaskInfo(item, 'waitingQueue'));
-            stoppedQueue.forEach(item => printTaskInfo(item, 'stoppedQueue'));
+            var active = activeQueue ? activeQueue.map(item => printTaskInfo(item)) : [];
+            var waiting = waitingQueue ? waitingQueue.map(item => printTaskInfo(item)) : [];
+            var stopped = stoppedQueue ? stoppedQueue.map(item => printTaskInfo(item)) : [];
+            document.getElementById('activeQueue').innerHTML = active.join('');
+            document.getElementById('waitingQueue').innerHTML = waiting.join('');
+            document.getElementById('stoppedQueue').innerHTML = stopped.join('');
         });
     }
 
-    function printTaskInfo(result, target) {
-        var task = document.getElementById(result.gid);
-        if (task) {
-            if (task.value !== 'complete' && result.status === 'complete') {
-                task.remove();
-                createTaskInfo();
-            }
-            else {
-                document.getElementById(result.gid).contentWindow.postMessage(result);
-            }
-        }
-        else {
-            createTaskInfo();
-        }
-
-        function createTaskInfo() {
-            openModuleWindow({'name': 'template', 'id': result.gid, 'value': result.status, 'target': target, 'load': (event) => event.target.contentWindow.postMessage(result)});
-        }
+    function printTaskInfo(result) {
+        var downloadSpeed = bytesToFileSize(result.downloadSpeed);
+        var totalLength = bytesToFileSize(result.totalLength);
+        var completedLength = bytesToFileSize(result.completedLength);
+        var estimatedTime = numberToTimeFormat((result.totalLength - result.completedLength) / result.downloadSpeed);
+        var completeRatio = ((result.completedLength / result.totalLength * 10000 | 0) / 100).toString() + '%';
+        var taskUrl = !result.bittorrent ? result.files[0].uris[0].uri : '';
+        var taskName = result.bittorrent && result.bittorrent.info ? result.bittorrent.info.name : result.files[0].path.split('/').pop() || taskUrl;
+        var connections = result.bittorrent ? result.numSeeders + ' (' + result.connections + ')' : result.connections;
+        var uploadSpeed = result.bittorrent ? '⏫ ' + bytesToFileSize(result.uploadSpeed) + '/s' : '';
+        var retryButton = !result.bittorrent && ['error', 'removed'].includes(result.status) ? '<span id="retry_btn" class="button">♻️</span>' : '';
+        return  '<div class="taskInfo" gid="' + result.gid + '" status="' + result.status + '">'
+        +           '<div class="taskBody">'
+        +               '<div class="title">' + taskName + '</div>'
+        +               '<div><span>🖥️ ' + completedLength + '</span><span>⏲️ ' + estimatedTime + '</span><span>📦 ' + totalLength + '</span></div>'
+        +               '<div><span>📶 ' + connections + '</span><span>⏬ ' + downloadSpeed + '/s</span><span>' + uploadSpeed + '</span></div>'
+        +           '</div>'
+        +           '<div class="taskMenu"><span id="remove_btn" class="button">❌</span><span id="invest_btn" class="button">🔍</span>' + retryButton + '</div>'
+        +           '<div id="progress_btn" class="fancybar ' + result.status + 'Bar"><span id="progress_btn" class="' + result.status + '" style="width: ' + completeRatio + '">' + completeRatio + '</span></div>'
+        +       '</div>'
     }
 }
 
