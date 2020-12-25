3a2edb899880d5b457825b7eb6f88b08e5f5128a
jc3213 <jc3213@users.noreply.github.com>
2020-12-25 14:57:02 +0800

    optimized performance and code style

--- modules/options/js.js
+++ modules/options/js.js
@@ -3,46 +3,45 @@ var menuTabs = [
     {button: 'tabAdvanced', queue: 'menuAdvanced'},
     {button: 'tabDownload', queue: 'menuDownload'}
 ];
-menuTabs.forEach(item => document.getElementById(item.button).addEventListener('click', (event) => toggleMenuTab(item)));
-
-function toggleMenuTab(active) {
-    document.getElementById(active.button).classList.add('checked');
-    document.getElementById(active.queue).style.display = 'block';
-    menuTabs.forEach(item => { if (item.queue !== active.queue) {document.getElementById(item.queue).style.display = 'none'; document.getElementById(item.button).classList.remove('checked');} });
-}
+menuTabs.forEach(active => {
+    document.getElementById(active.button).addEventListener('click', (event) => {
+        document.getElementById(active.button).classList.add('checked');
+        document.getElementById(active.queue).style.display = 'block';
+        menuTabs.forEach(item => { if (item.queue !== active.queue) {document.getElementById(item.queue).style.display = 'none'; document.getElementById(item.button).classList.remove('checked');} });
+    });
+});
 
 [
     {id: 'jsonrpc', value: 'http://localhost:6800/jsonrpc'},
     {id: 'token', value: ''},
-    {id: 'folder', value: 0, load: downloadFolder, change: downloadFolder},
+    {id: 'folder', value: 0, change: downloadFolder, onload: downloadFolder},
     {id: 'directory', value: ''},
     {id: 'useragent', value: navigator.userAgent},
     {id: 'allproxy', value: ''},
     {id: 'proxied', value: ''},
-    {id: 'capture', value: 0, load: captureFilters, change: captureFilters},
+    {id: 'capture', value: 0, change: captureFilters, onload: captureFilters},
     {id: 'sizeEntry', value: 0, change: calcFileSize},
     {id: 'sizeUnit', value: 2, change: calcFileSize},
     {id: 'fileExt', value: ''},
     {id: 'monitored', value: ''},
     {id: 'ignored', value: ''}
-].forEach(item => initiateOption(document.getElementById(item.id), item));
-
-function initiateOption(setting, property) {
+].forEach(property => {
+    var menu = document.getElementById(property.id);
     if (property.change) {
-        setting.addEventListener('change', property.change);
+        menu.addEventListener('change', property.change);
     }
     if (property.checkbox) {
-        setting.setAttribute('checked', JSON.parse(localStorage.getItem(property.id)) || property.value);
-        setting.addEventListener('change', event => localStorage.setItem(property.id, event.target.checked));
+        menu.setAttribute('checked', JSON.parse(localStorage.getItem(property.id)) || property.value);
+        menu.addEventListener('change', (event) => localStorage.setItem(property.id, event.target.checked));
     }
     else {
-        setting.value = localStorage.getItem(property.id) || property.value
-        setting.addEventListener('change', event => localStorage.setItem(property.id, event.target.value));
+        menu.value = localStorage.getItem(property.id) || property.value;
+        menu.addEventListener('change', (event) => localStorage.setItem(property.id, event.target.value));
     }
-    if (property.load) {
-        property.load();
+    if (typeof property.onload === 'function') {
+        property.onload();
     }
-}
+});
 
 document.getElementById('aria2Check').addEventListener('click', (event) => {
     jsonRPCRequest(
@@ -67,7 +66,7 @@ document.getElementById('aria2Show').addEventListener('click', (event) => {
 });
 
 function downloadFolder() {
-    var folder = (document.getElementById('folder').value | 0);
+    var folder = document.getElementById('folder').value | 0;
     if (folder === 2) {
         document.getElementById('directory').style.display = 'block';
     }
@@ -77,7 +76,7 @@ function downloadFolder() {
 }
 
 function captureFilters() {
-    var capture = (document.getElementById('capture').value | 0);
+    var capture = document.getElementById('capture').value | 0;
     if (capture === 1) {
         document.getElementById('captureFilters').style.display = 'block';
     }
@@ -87,8 +86,8 @@ function captureFilters() {
 }
 
 function calcFileSize(event) {
-    var number = (document.getElementById('sizeEntry').value | 0);
-    var unit = (document.getElementById('sizeUnit').value | 0);
+    var number = document.getElementById('sizeEntry').value | 0;
+    var unit = document.getElementById('sizeUnit').value | 0;
     var size = number * Math.pow(1024, unit);
     localStorage.setItem('fileSize', size);
 }
